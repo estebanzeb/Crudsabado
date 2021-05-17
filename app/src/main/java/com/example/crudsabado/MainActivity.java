e7ec596090e02b206a0331519b4b8d688200ffdd
estebanzeb <estebanzeb@gmail.com>
2021-05-17 17:09:41 -0500

    Initial commit

---
+++ app/src/main/java/com/example/crudsabado/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.crudsabado;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    
+    //Atributos
+    
+    Edittext
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
